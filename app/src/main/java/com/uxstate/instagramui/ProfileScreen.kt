6c86016141c4e021d5649544b2290f1e1bd6d0a1
Antony <vontonnie@gmail.com>
2022-02-10 07:33:50 +0300

    Add Profile Screen Composable

--- app/src/main/java/com/uxstate/instagramui/ProfileScreen.kt
+++ app/src/main/java/com/uxstate/instagramui/ProfileScreen.kt
@@ -1,2 +1,13 @@
 package com.uxstate.instagramui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        
+    }
+}
