7b8561f5514e1fb7725b3c0b10c2e4560b1adf51
Antony <vontonnie@gmail.com>
2022-02-10 07:57:49 +0300

    Add Topbar Children

--- app/src/main/java/com/uxstate/instagramui/ProfileScreen.kt
+++ app/src/main/java/com/uxstate/instagramui/ProfileScreen.kt
@@ -1,12 +1,20 @@
 package com.uxstate.instagramui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ProfileScreen() {
@@ -16,12 +24,44 @@ fun ProfileScreen() {
 }
 
 @Composable
-fun TopBar() {
+fun TopBar(name: String, modifier: Modifier = Modifier) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround,
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
 
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "Back",
+            modifier = Modifier.size(24.dp),
+            tint = Color.Black
+        )
+        Text(
+            text = name,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            overflow = TextOverflow.Ellipsis
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_bell),
+            contentDescription = "Sub",
+            tint = Color.Black
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_dotmenu),
+            contentDescription = "Menu",
+            tint = Color.Black
+        )
+
+    }
+}
+
+@Preview(name = "MyPreview")
+@Composable
+fun MyPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        TopBar(name = "Tonnie_Dev")
     }
 }
