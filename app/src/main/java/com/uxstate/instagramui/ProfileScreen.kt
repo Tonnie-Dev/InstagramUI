1814d4ec4d31ac319421bc24273a4b9b629e1137
Antony <vontonnie@gmail.com>
2022-02-10 07:36:39 +0300

    Add Topbar Composable

--- app/src/main/java/com/uxstate/instagramui/ProfileScreen.kt
+++ app/src/main/java/com/uxstate/instagramui/ProfileScreen.kt
@@ -1,13 +1,27 @@
 package com.uxstate.instagramui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun ProfileScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
-        
+
+    }
+}
+
+@Composable
+fun TopBar() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
     }
 }
